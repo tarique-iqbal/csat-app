772baf110676471ad1523c11ca195e27c4e2001b
Tarique Iqbal <live.iqbal@gmail.com>
2025-06-28 22:57:27 +0200

    feat: add config/migrations.php

---
+++ api-service/config/migrations.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'table_storage' => [
+        'table_name' => 'migration_versions',
+    ],
+
+    'migrations_paths' => [
+        'Migrations' => BASE_DIR . '/migrations',
+    ],
+];
