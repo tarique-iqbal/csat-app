0067da87ae74374d62d15a23f8d2a88448fbb42e
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-04 21:12:38 +0200

    feat(domain): add CsatWeeklyScore value object

---
+++ api-service/src/Domain/Csat/ValueObject/CsatWeeklyScore.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Csat\ValueObject;
+
+final readonly class CsatWeeklyScore
+{
+    public function __construct(
+        public UserId $userId,
+        public Score $score,
+        public Week $week,
+        public Year $year,
+    ) {
+    }
+}
