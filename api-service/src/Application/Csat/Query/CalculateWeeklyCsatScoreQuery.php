8a72b93f39c30f052b986f9045747a9ec01fb5d9
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-10 13:58:05 +0200

    chore: add CalculateWeeklyCsatScoreQuery

---
+++ api-service/src/Application/Csat/Query/CalculateWeeklyCsatScoreQuery.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Csat\Query;
+
+use App\Domain\Csat\ValueObject\WeekOfYear;
+
+final readonly class CalculateWeeklyCsatScoreQuery
+{
+    public function __construct(
+        public WeekOfYear $weekOfYear,
+    ) {
+    }
+}
