1bd8c1637c17f94943bb7d79ef78e48776acda07
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-11 23:25:37 +0200

    chore: add autowire for CsatRepositoryInterface

--- api-service/config/container.php
+++ api-service/config/container.php
@@ -2,12 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Domain\Csat\Repository\CsatRepositoryInterface;
+use App\Infrastructure\Persistence\Dbal\DbalCsatRepository;
 use DI\ContainerBuilder;
 use Doctrine\DBAL\Connection;
 use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
+use function DI\autowire;
 
 $builder = new ContainerBuilder();
 
@@ -20,6 +23,7 @@ $builder->addDefinitions([
         $logger->pushHandler(new StreamHandler(BASE_DIR . '/var/log/error.log', Level::Debug));
         return $logger;
     },
+    CsatRepositoryInterface::class => autowire(DbalCsatRepository::class),
 ]);
 
 return $builder->build();
