ad8233dc75714975174bcf5379c1049872a43d10
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-16 19:45:29 +0200

    chore: add test for CalculateWeeklyCsatScoreUseCase

---
+++ api-service/tests/Integration/Application/Csat/UseCase/CalculateWeeklyNpsScoreUseCaseTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Application\Csat\UseCase;
+
+use App\Application\Csat\Query\CalculateWeeklyCsatScoreQuery;
+use App\Application\Csat\UseCase\CalculateWeeklyCsatScoreUseCase;
+use App\Domain\Csat\ValueObject\WeekOfYear;
+use App\Infrastructure\Persistence\Dbal\DbalCsatRepository;
+use App\Infrastructure\Persistence\Schema\Tables;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\Infrastructure\Database\IntegrationTestCase;
+
+final class CalculateWeeklyNpsScoreUseCaseTest extends IntegrationTestCase
+{
+    private CalculateWeeklyCsatScoreUseCase $useCase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $repository = new DbalCsatRepository($this->connection);
+        $this->useCase = new CalculateWeeklyCsatScoreUseCase($repository);
+    }
+
+    public static function weekly_score_provider(): array
+    {
+        return [
+            [
+                [
+                    [1, 5, 21, 2024],
+                    [2, 3, 21, 2024],
+                    [3, 4, 21, 2024],
+                ],
+                66.67,
+            ],
+            [
+                [
+                    [1, 5, 21, 2024],
+                    [2, 4, 21, 2024],
+                    [3, 4, 21, 2024],
+                ],
+                100.00,
+            ],
+            [
+                [], 0.0
+            ],
+        ];
+    }
+
+    #[DataProvider('weekly_score_provider')]
+    public function test_calculates_correct_nps_score(array $weeklyScore, float $expectedNpsScore): void
+    {
+        $this->insertSampleScores($weeklyScore);
+
+        $weekOfYear = new WeekOfYear(21, 2024);
+        $query = new CalculateWeeklyCsatScoreQuery($weekOfYear);
+        $score = $this->useCase->execute($query);
+
+        $this->assertSame($expectedNpsScore, $score);
+    }
+
+    private function insertSampleScores(array $rows): void
+    {
+        foreach ($rows as [$userId, $score, $week, $year]) {
+            $this->connection->insert(Tables::SCORES, [
+                'user_id' => $userId,
+                'score' => $score,
+                'week' => $week,
+                'year' => $year,
+            ]);
+        }
+    }
+}
