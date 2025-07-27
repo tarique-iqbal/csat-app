928f72775211cf1a598044c27dcc29c354c0386b
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-27 22:27:28 +0200

    feat: add reusable nav component

---
+++ web-user/src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
