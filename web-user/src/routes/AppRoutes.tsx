29a4a6b43992c70ae5226aeb6160ba69f6aadde3
Tarique Iqbal <live.iqbal@gmail.com>
2025-07-28 15:20:39 +0200

    feat: add routes in AppRoutes.tsx

---
+++ web-user/src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+const AppRoutes: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRoutes;
