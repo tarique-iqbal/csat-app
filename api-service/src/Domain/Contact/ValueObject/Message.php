1bf9e7eca4f9910a192196ff005a975a3da438d5
Tarique Iqbal <live.iqbal@gmail.com>
2025-08-27 23:08:12 +0200

    feat(domain): add Message value object

---
+++ api-service/src/Domain/Contact/ValueObject/Message.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Contact\ValueObject;
+
+use App\Domain\Contact\Exception\ContactMessageException;
+
+final class Message
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $value = trim($value);
+
+        if ($value === '') {
+            throw new ContactMessageException('Message cannot be empty');
+        }
+
+        if (mb_strlen($value) < 100) {
+            throw new ContactMessageException('Message must be at least 100 characters long');
+        }
+
+        if (mb_strlen($value) > 2000) {
+            throw new ContactMessageException('Message must not exceed 2000 characters');
+        }
+
+        $this->value = $value;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
